61b049bfedf72c083dad5b2674d31d6c74794d84
Felipe Navas <felipenavas.itec@gmail.com>
2022-09-22 08:55:19 -0300

    Adding router, and pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BowserRouter } from 'react-router-dom'
+
 import { HeroesApp } from './HeroesApp'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HeroesApp />
+    <BowserRouter>
+      <HeroesApp />
+    </BowserRouter>
   </React.StrictMode>
 )
