f6a068c96303f27290a5c7df14263d0c09273750
Felipe Navas <felipenavas.itec@gmail.com>
2022-09-26 08:52:01 -0300

    Adding Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import { HeroesApp } from './HeroesApp'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BowserRouter>
+    <BrowserRouter>
       <HeroesApp />
-    </BowserRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
