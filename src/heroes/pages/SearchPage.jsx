81ffaa9982de4bcc0deb8c5401986fc7dfa4edb8
Felipe Navas <felipenavas.itec@gmail.com>
2022-10-21 08:50:15 -0300

    Adding context, provider and reducer

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -22,8 +22,6 @@ export const SearchPage = () => {
   const onSearchSubmit = (e) => {
     e.preventDefault()
 
-    // if (searchText.trim().length <= 1) return
-
     navigate(`?q=${searchText}`)
   }
 
@@ -53,16 +51,6 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Results</h4>
           <hr />
-
-          {/* {q === '' ? (
-            <div className="alert alert-primary">Search a hero</div>
-          ) : (
-            heroes.length === 0 && (
-              <div className="alert alert-danger">
-                No hero with <b>"{q}"</b>
-              </div>
-            )
-          )} */}
 
           <div
             className="alert alert-primary animate__animated animate__fadeIn"
