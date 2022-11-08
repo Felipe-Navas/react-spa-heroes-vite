bfaea4cd15ae1006571667a631807ca11f0c5e2d
Felipe Navas <felipenavas.itec@gmail.com>
2022-11-08 08:56:13 -0300

    Adding tests to SearchPage

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -34,7 +34,7 @@ export const SearchPage = () => {
         <div className="col-5">
           <h4>Searching</h4>
           <hr />
-          <form onSubmit={onSearchSubmit}>
+          <form onSubmit={onSearchSubmit} aria-label="form">
             <input
               className="form-control"
               type="text"
@@ -60,6 +60,7 @@ export const SearchPage = () => {
           </div>
 
           <div
+            aria-label="alert-danger"
             className="alert alert-danger animate__animated animate__fadeIn"
             style={{ display: showError ? '' : 'none' }}
           >
